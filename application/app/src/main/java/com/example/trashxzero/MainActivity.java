cb4b9bd05e4d150487aae6447f80a8e9bb39da3b
shash <shashankshazz28@gmail.com>
2025-05-04 22:26:06 +0530

    Add app files to repo

---
+++ application/app/src/main/java/com/example/trashxzero/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.trashxzero;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://binxzero-7d76f.web.app");
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
